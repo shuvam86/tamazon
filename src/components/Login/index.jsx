ec6151375244030fc2756416e38d1e33bffd719e
shuvam86 <shubhamguptakmr@gmail.com>
2025-01-08 16:01:06 +0530

    added login page

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
 
 const Login = () => {
-  return (
-    <div>
-      From login page
-    </div>
-  )
-}
+    return (
+        <Container maxWidth="sm">
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="100vh"
+            >
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Login
+                </Typography>
+                <form noValidate autoComplete="off">
+                    <TextField
+                        label="Username"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                    />
+                    <TextField
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                        style={{ marginTop: '16px' }}
+                    >
+                        Login
+                    </Button>
+                </form>
+            </Box>
+        </Container>
+    );
+};
 
-export default Login
+export default Login;
